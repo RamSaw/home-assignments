620a3e353f12423c44d093b10e1a78485789ab26
Mikhail Pravilov <prav.mischa@gmail.com>
2020-02-10 19:28:23 +0300

    initial version without adding new points

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -36,16 +36,52 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
+    # params for ShiTomasi corner detection
+    feature_params = dict(maxCorners=500,
+                          qualityLevel=0.01,
+                          minDistance=7,
+                          blockSize=7)
+
+    # Parameters for lucas kanade optical flow
+    lk_params = dict(winSize=(15, 15),
+                     maxLevel=2,
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+    # Take first frame and find corners in it
     image_0 = frame_sequence[0]
+    #image_0_gray = cv2.cvtColor(image_0, cv2.COLOR_BGR2GRAY)
+    p0 = cv2.goodFeaturesToTrack(image_0, mask=None, **feature_params).squeeze()
+    ids = np.arange(len(p0))
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        ids=ids,
+        points=p0,
+        sizes=np.array([feature_params['blockSize'] for i in range(len(ids))])
     )
     builder.set_corners_at_frame(0, corners)
+
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(image_0)
+
+    image_0 = np.uint8(image_0 * 255)
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        image_1 = np.uint8(image_1 * 255)
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(image_0, image_1, p0, None, **lk_params)
+        # Select good points
+        detected_mask = (st == 1).squeeze()
+
+        good_new = p1[detected_mask]
+        ids = ids[detected_mask]
+
+        corners = FrameCorners(
+            ids=ids,
+            points=good_new,
+            sizes=np.array([feature_params['blockSize'] for i in range(len(ids))])
+        )
         builder.set_corners_at_frame(frame, corners)
+
         image_0 = image_1
+        p0 = p1
 
 
 def build(frame_sequence: pims.FramesSequence,
